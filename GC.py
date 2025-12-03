d49f69f4b7cebe1baa0b7604e2af65728dcae1e7
j-homola <149617157+j-homola@users.noreply.github.com>
2025-12-03 17:59:36 -0600

    Change file path for input sequence file
    Updated file path for reading sequences from a file.

--- GC.py
+++ GC.py
@@ -1,16 +1,16 @@
-with open('/Users/Julia/Downloads/rosalind_gc-2.txt', 'r') as f:
+file_path = 'your path here'
+with open(file_path, 'r') as f:
     seqs = f.read().split(">")
 
-# print(seqs)
 max_id = ""
 max_gc = 0
+
 for i in range(1,len(seqs)):
     id, s = seqs[i].split("\n", 1)
-    # print(len(s))
     s = s.replace("\n", "")
-    # print(len(s))
     gc = s.count("G") + s.count("C")
     gc_pct = gc / len(s.strip()) * 100
+    
     if gc_pct > max_gc:
         max_id = id
         max_gc = gc_pct
