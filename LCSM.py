c58f35a0f88f397f301851ae03de66197fc483e3
j-homola <149617157+j-homola@users.noreply.github.com>
2025-12-03 18:43:28 -0600

    Modify fasta file path and improve match logic
    Updated file path for fasta input and fixed indentation.

--- LCSM.py
+++ LCSM.py
@@ -1,9 +1,10 @@
 import fasta_reader as fr
 
-seq_dict = fr.read_fasta('/Users/Julia/Downloads/rosalind_lcsm.txt')
+file_path = 'your path here'
+seq_dict = fr.read_fasta(file_path)
 
 values = list(seq_dict.values())
-# print(values)
+
 def find_matches(vals, k):
     for i in range(len(vals[0])-k):
         motif = vals[0][i:i+k]
@@ -11,7 +12,7 @@ def find_matches(vals, k):
         for v in vals[1:]:
             if motif not in v:
                 match = False
-                break
+                break   
         if match:
             return motif
     return None
