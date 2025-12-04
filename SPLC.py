4745385643f925546dd472a079792bb338ebae7d
j-homola <149617157+j-homola@users.noreply.github.com>
2025-12-03 18:54:10 -0600

    Modify FASTA file path in SPLC.py
    Updated file path for reading FASTA file.

--- SPLC.py
+++ SPLC.py
@@ -1,11 +1,14 @@
 import fasta_reader as fr
 import dna_functions as dnaf
 
-keys, vals = fr.read_fasta('/Users/Julia/Downloads/rosalind_splc.txt', format=list)
+file_path = 'your path here'
+
+keys, vals = fr.read_fasta(file_path, format=list)
 strand = vals[0]
 
 for seq in vals[1:]:
     strand = strand.replace(seq, '')
 
 r_strand = dnaf.dna_to_rna(strand)
+
 print(dnaf.translate(r_strand))
