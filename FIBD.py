b904d13552e530cb51b0a4d52f49fe9aecc4045a
j-homola <149617157+j-homola@users.noreply.github.com>
2025-12-03 18:37:58 -0600

    Refactor file path handling in FIBD.py

--- FIBD.py
+++ FIBD.py
@@ -1,14 +1,16 @@
-with open('/Users/Julia/Downloads/rosalind_fibd.txt', 'r') as f:
+file_path = 'your path here'
+
+with open(file_path, 'r') as f:
     n, m = f.read().split()
 # n, m = [6, 3]
 n = int(n)
 m = int(m)
 new_pairs = [1]
+
 for i in range(n-1):
     if i <= m:
         new_pairs.append(sum(new_pairs[:i]))
     else:
         new_pairs.append(sum(new_pairs[i-m+1:i]))
     
-# print(new_pairs)
 print(sum(new_pairs[n-m+1:]))
