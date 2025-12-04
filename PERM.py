3496d2a6c3421e698703fbdf83aed0023bab13d9
j-homola <149617157+j-homola@users.noreply.github.com>
2025-12-03 18:51:16 -0600

    Refactor file path for input in PERM.py
    Updated file path handling for reading input.

--- PERM.py
+++ PERM.py
@@ -1,6 +1,8 @@
 import itertools
 
-with open('/Users/Julia/Downloads/rosalind_perm-2.txt', 'r') as f:
+file_path = 'your path here'
+
+with open(file_path, 'r') as f:
     n = int(f.read())
 # n = 3
 perms = list(itertools.permutations(range(1, n+1)))
